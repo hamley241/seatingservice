481bb551dadb1032257531f25306d5b13afed6f6
hamley241 <gpatley@gmail.com>
2019-09-23 16:38:34 -0400

    working for non-consecutive allocation

--- seatingservice/models/show.py
+++ seatingservice/models/show.py
@@ -3,6 +3,7 @@ from copy import deepcopy
 from models.seat import Seat
 import re
 import statistics
+import numpy as np
 
 class Show(object):
     def __init__(self, name="default", movie_name=None, timings=None):
@@ -19,7 +20,7 @@ class Show(object):
         seats_config = Config.get_data_map().get("theatre").get(self.get_name()).get("seats_matrix")
         seats_matrix = []
         print(seats_config)
-        for cnt in range(0, len(seats_config)):
+        for cnt in reversed(range(0, len(seats_config))):
             seats_matrix.append(self._init_row_seats(cnt, seats_config))
         return seats_matrix
 
@@ -41,7 +42,7 @@ class Show(object):
         seats_names = []
         for row in seats_:
             seats_names.append(str(self._get_row_seat_names(row)))
-        return "\tSCREEN this way\n{}".format("\n".join(seats_names))
+        return "{}\n\tSCREEN this way\n".format("\n".join(seats_names))
 
     def _get_row_seat_names(self, seats_row):
         return [str(item) for item in seats_row]
@@ -50,15 +51,17 @@ class Show(object):
         # Get list of things that are vacant row wise
         empty_seats = []
         for row in self.get_seats():
-            empty_seats.append([str(int(seat.is_empty())) for seat in row])
+            empty_seats.append([int(seat.is_empty()) for seat in row])
         return empty_seats
 
-    def find_consecutive_empty_seats(self, num_of_seats):
-        empty_seats = self.get_empty_seats()
+    def get_empty_seats_count(self):
+        return np.array(self.get_empty_seats()).sum()
+
+    def find_consecutive_empty_seats(self, num_of_seats, empty_seats):
         consecutive_string = "".join(["1"]*num_of_seats)
         consecutive_re = re.compile(consecutive_string)
         for cnt, row in enumerate(empty_seats):
-            row_string = "".join(row)
+            row_string = "".join([str(item) for item in row])
             results = consecutive_re.finditer(row_string)
             results = list(results)
             if results:
@@ -78,14 +81,23 @@ class Show(object):
 
     def find_best_seats(self, num_of_seats):
         # Use Regex to find consecutive empty seats
-        return self.find_consecutive_empty_seats(num_of_seats)
+        empty_seats = self.get_empty_seats()
+        selected_seats =  self.find_consecutive_empty_seats(num_of_seats, empty_seats)
+        if not selected_seats:
+            selected_seats = self.find_nonconsecutive_empty_seats(num_of_seats, empty_seats)
+        return selected_seats
 
     def book(self, seats_list=None,num_seats=0):
         if seats_list:
             return self._book_seats_list(seats_list)
         elif num_seats:
             seats_list = self.find_best_seats(num_seats)
-            return self._book_seats_list(seats_list)
+            if seats_list:
+                print("CAN assign")
+                return self._book_seats_list(seats_list)
+            else:
+                print("Cant assign")
+                return []
         else:
             #Raise an exception here
             return
@@ -106,13 +118,37 @@ class Show(object):
             return False
         return booked_list
 
+    def find_nonconsecutive_empty_seats(self, num_of_seats, empty_seats):
+        empty_seats_arr =  np.array(empty_seats)
+        rows_sum = empty_seats_arr.sum(axis=1)
+        if np.sum(rows_sum) < num_of_seats:
+            print("Cannot assign seats ")
+            return []
+        elif np.max(rows_sum) >= num_of_seats:
+            row_indx = np.argmax(rows_sum>=num_seats)
+            col_indices = (np.where(empty_seats_arr[row_indx] == 1)[0]).tolist()[:num_of_seats]
+            print("DEBUG Assigning in a single row {}".format((str(num_of_seats))))
+            return [s.get_seats()[row_indx][col_indx] for col_indx in col_indices]
+        else:
+            print("DEBUG Cannot assign in a single row {}".format((str(num_of_seats))))
+            row_indexes, col_indices = np.where(empty_seats_arr == 1)
+            return [s.get_seats()[row_indexes[cnt]][col_indices[cnt]] for cnt, item in enumerate(col_indices)][:num_of_seats]
+
+
+
+
+
 
 if __name__ == "__main__":
+    import random
     s = Show()
     print(s)
-    for i in range(1,25):
-        bs = s.book(num_seats=4)
+    for i in range(1,35):
+        # num_seats = 4 if i <= 20 else random.randint(1,6)
+        num_seats = random.randint(1,6)
+        print("Seats requested {}".format(str(num_seats)))
+        bs = s.book(num_seats=num_seats)
         print("\t".join([str(item) for item in  bs]))
-        print(s.get_empty_seats())
+        print("Empty seats count {}\n##########\n".format(s.get_empty_seats_count()))
     # bs = s.book(num_seats=4)
     # print("\t".join([str(item) for item in  bs]))
