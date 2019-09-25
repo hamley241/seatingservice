dcd7cc5eb1fcc94bf937dd83543b2508a70fc461
hamley241 <gpatley@gmail.com>
2019-09-25 02:30:23 -0400

    Minor updates to the assignment sript

--- seatingservice/seat_assignment_script.py
+++ seatingservice/seat_assignment_script.py
@@ -4,9 +4,10 @@ from app.controllers import Seats
 import argparse
 
 import datetime
-# import logging
-from utils.logs import  Logger
+from utils.logs import Logger
+
 logging = Logger.get_logger()
+
 
 class InvalidEventException(Exception):
     pass
@@ -25,7 +26,7 @@ class SeatRequestEvent:
         return self._seats_count
 
     def to_dict(self):
-        return dict(txnId =self.get_txn_id(), seatsCount=self.get_seats_count())
+        return dict(txnId=self.get_txn_id(), seatsCount=self.get_seats_count())
 
 
 class SeatAssigner:
@@ -34,10 +35,11 @@ class SeatAssigner:
         self.assign_service = seats_asssginig_service
         self.consumer = event_consumer  # can have a wrapped consumer to be always in consistent with what is expected
         self.event_producer = event_producer
+
     def process(self):
         for event in self.consumer:
             try:
-                logging.info(" {} Received Event {}".format(datetime.datetime.utcnow(),str(event)))
+                logging.info(" {} Received Event {}".format(datetime.datetime.utcnow(), str(event)))
                 event = self.validate_event(event)
                 response = self.assign_service.post(request_params=event.to_dict())
                 if response.get("status") == "success":
@@ -45,9 +47,8 @@ class SeatAssigner:
                 else:
                     logging.error("No seat was assigned to request event {}".format(event.to_dict()))
             except InvalidEventException as e:
-                logging.error("Invalid event {} exception {}".format(str(event), e))
+                logging.error("Invalid event {} exception {}".format(str(event).strip("\n"), e))
             print("Result")
-
 
     def validate_event(self, event):
         if not event:
@@ -66,9 +67,7 @@ class SeatAssigner:
         if not txn_id.startswith("R"):
             raise InvalidEventException()
 
-
-        return SeatRequestEvent(txn_id,seats_count)
-
+        return SeatRequestEvent(txn_id, seats_count)
 
 
 if __name__ == "__main__":
@@ -76,12 +75,17 @@ if __name__ == "__main__":
     # parser.add_argument('--input', '-i', dest='inputfile', help='Complete path to Input file containing the reservation requests', required=True,action='store_true')
     # opts= parser.parse_args()
     # print(opts)
-    if len(sys.argv) !=2:
-        print("seat_assignment_script.py: error: the following arguments are required: inputfile \n ex: {} inputfile.txt".format("seat_assignment_script.py"))
+    if len(sys.argv) != 2:
+        print(
+            "seat_assignment_script.py: error: the following arguments are required: inputfile \n ex: {} inputfile.txt".format(
+                "seat_assignment_script.py"))
+        os._exit(1)
     inputfile = sys.argv[1]
-    requests_file = open(inputfile,"r")
-    response_file = open("responses.txt", "w")
-    sample_event = ["R001 2","R002 4"]
+    requests_file = open(inputfile, "r")
+    response_file_name = "responses.txt"
+    response_file = open(response_file_name, "w")
+    sample_event = ["R001 2", "R002 4"]
     seats_asssginig_service = Seats()
-    seat_assigner = SeatAssigner(seats_asssginig_service,requests_file, response_file)
+    seat_assigner = SeatAssigner(seats_asssginig_service, requests_file, response_file)
     seat_assigner.process()
+    print("Responses can be found at {}".format(os.path.abspath(response_file_name)))
